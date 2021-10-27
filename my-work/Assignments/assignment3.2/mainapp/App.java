c38186565abf491727f7f9ffe844a8ceb0448564
varun2awesome <varun2awesome@gmail.com>
2021-10-28 00:29:33 +0530

    Uploading Assignment3.2

---
+++ my-work/Assignments/assignment3.2/mainapp/App.java
@@ -0,0 +1,25 @@
+package in.assignment3.2.mainapp;
+
+package in.assignment3.2.finance;.ATM;
+package in.assignment3.2.finance;.Bank;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bank B=new Bank(9801,"pa@ss",7700);
+
+		ATM A=new ATM();
+		
+		A.withdraw(9801,"p@ss",7700);
+		A.Deposit(9801, 9000);
+		
+		A.changePassword("p@ss","varun@");
+		
+
+		
+		
+	}
+
+}
